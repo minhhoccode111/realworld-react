1f8d998b808095a9748d2423f9dff7581dfa731c
minhhoccode111 <minhhoccode111@gmail.com>
2025-11-10 21:35:55 +0700

    refactor: settings ui

--- src/features/settings/components/settings-form.tsx
+++ src/features/settings/components/settings-form.tsx
@@ -1,35 +1,23 @@
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-
-import { useUser } from '@/lib/auth';
+import { Form } from '@/components/ui/form';
+import { useNotifications } from '@/components/ui/notifications';
 import {
   updateProfileInputSchema,
   useUpdateProfile,
 } from '@/features/settings/api/update-profile';
-import { useNotifications } from '@/components/ui/notifications';
+import { useUser } from '@/lib/auth';
 
-export const SettingsForm = () => {
+type SettingsFormProps = {
+  onSuccess?: () => void;
+};
+
+export const SettingsForm = ({ onSuccess }: SettingsFormProps) => {
   const user = useUser();
   const { addNotification } = useNotifications();
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: zodResolver(updateProfileInputSchema),
-    defaultValues: {
-      email: user.data?.user.email ?? '',
-      username: user.data?.user.username ?? '',
-      bio: user.data?.user.bio ?? '',
-      image: user.data?.user.image ?? '',
-      password: '',
-    },
-  });
 
   const updateProfileMutation = useUpdateProfile({
     mutationConfig: {
       onSuccess: () => {
+        onSuccess?.();
         addNotification({
           type: 'success',
           title: 'Profile Updated',
@@ -38,78 +26,80 @@ export const SettingsForm = () => {
     },
   });
 
-  const onSubmit = handleSubmit((values) => {
-    updateProfileMutation.mutate({
-      data: {
-        email: values.email,
-        username: values.username,
-        bio: values.bio,
-        image: values.image,
-        password: values.password,
-      },
-    });
-  });
-
   return (
-    <form onSubmit={onSubmit}>
-      <fieldset>
-        <fieldset className="form-group">
-          <input
-            className="form-control"
-            type="text"
-            placeholder="URL of profile picture"
-            {...register('image')}
-          />
-        </fieldset>
-        <fieldset className="form-group">
-          <input
-            className="form-control form-control-lg"
-            type="text"
-            placeholder="Your Username"
-            {...register('username')}
-          />
-        </fieldset>
-        <fieldset className="form-group">
-          <textarea
-            className="form-control form-control-lg"
-            rows={8}
-            placeholder="Short bio about you"
-            {...register('bio')}
-          ></textarea>
-        </fieldset>
-        <fieldset className="form-group">
-          <input
-            className="form-control form-control-lg"
-            type="text"
-            placeholder="Email"
-            {...register('email')}
-          />
-        </fieldset>
-        <fieldset className="form-group">
-          <input
-            className="form-control form-control-lg"
-            type="password"
-            placeholder="New Password"
-            {...register('password')}
-          />
-        </fieldset>
+    <Form
+      onSubmit={(values) => updateProfileMutation.mutate(values)}
+      schema={updateProfileInputSchema}
+      options={{
+        defaultValues: {
+          image: user.data?.user.image ?? '',
+          username: user.data?.user.username ?? '',
+          bio: user.data?.user.bio ?? '',
+          email: user.data?.user.email ?? '',
+          password: '',
+        },
+      }}
+    >
+      {({ register, formState }) => (
+        <fieldset>
+          <fieldset className="form-group">
+            <input
+              className="form-control"
+              type="text"
+              placeholder="URL of profile picture"
+              {...register('image')}
+            />
+          </fieldset>
+          <fieldset className="form-group">
+            <input
+              className="form-control form-control-lg"
+              type="text"
+              placeholder="Your Username"
+              {...register('username')}
+            />
+          </fieldset>
+          <fieldset className="form-group">
+            <textarea
+              className="form-control form-control-lg"
+              rows={8}
+              placeholder="Short bio about you"
+              {...register('bio')}
+            ></textarea>
+          </fieldset>
+          <fieldset className="form-group">
+            <input
+              className="form-control form-control-lg"
+              type="text"
+              placeholder="Email"
+              {...register('email')}
+            />
+          </fieldset>
+          <fieldset className="form-group">
+            <input
+              className="form-control form-control-lg"
+              type="password"
+              placeholder="New Password"
+              {...register('password')}
+            />
+          </fieldset>
 
-        {Object.entries(errors).length > 0 && (
-          <ul className="error-messages">
-            {Object.entries(errors).map(([field, error]) => (
-              <li key={field}>{error?.message?.toString()}</li>
-            ))}
-          </ul>
-        )}
+          {Object.entries(formState.errors).length > 0 && (
+            <ul className="error-messages">
+              {Object.entries(formState.errors).map(([field, error]) => (
+                <li key={field}>{error?.message?.toString()}</li>
+              ))}
+            </ul>
+          )}
 
-        <button
-          className="btn btn-lg btn-primary pull-xs-right"
-          type="submit"
-          disabled={updateProfileMutation.isPending}
-        >
-          {updateProfileMutation.isPending ? 'Loading...' : 'Update Settings'}
-        </button>
-      </fieldset>
-    </form>
+          <button
+            className="btn btn-lg btn-primary pull-xs-right"
+            type="submit"
+            disabled={updateProfileMutation.isPending}
+          >
+            {updateProfileMutation.isPending ? 'Loading...' : 'Update Settings'}
+          </button>
+        </fieldset>
+      )}
+    </Form>
   );
 };
