6d8cfb3fb1975ab32492d3775b4bae2b313ef7b8
minhhoccode111 <minhhoccode111@gmail.com>
2025-11-12 21:41:41 +0700

    add: authorization policies

--- src/lib/authorization.tsx
+++ src/lib/authorization.tsx
@@ -1,12 +1,26 @@
 import * as React from 'react';
 
-import { UserAuth, CommentDetail } from '@/types/api';
+import { UserAuth, CommentDetail, ArticleDetail } from '@/types/api';
 
 // TODO: add role 'ADMIN' later to control users' content
 
 export const POLICIES = {
-  'comment:delete': (user: UserAuth, comment: CommentDetail) => {
-    if (comment.author.username === user.username) {
+  'article:edit': (user?: UserAuth, article?: ArticleDetail) => {
+    if (article?.author.username === user?.username) {
+      return true;
+    }
+
+    return false;
+  },
+  'article:delete': (user?: UserAuth, article?: ArticleDetail) => {
+    if (article?.author.username === user?.username) {
+      return true;
+    }
+
+    return false;
+  },
+  'comment:delete': (user?: UserAuth, comment?: CommentDetail) => {
+    if (comment?.author.username === user?.username) {
       return true;
     }
 
