d36151262a356cb4ecff470246e472a8069d1b3f
minhhoccode111 <minhhoccode111@gmail.com>
2025-11-12 20:50:39 +0700

    add: api get all comments of an article feature

---
+++ src/features/comments/api/get-comments.ts
@@ -0,0 +1,58 @@
+import { infiniteQueryOptions, useInfiniteQuery } from '@tanstack/react-query';
+
+import { api } from '@/lib/api-client';
+import { QueryConfig } from '@/lib/react-query';
+import { CommentDetailsResponse } from '@/types/api';
+
+const LIMIT = 10;
+const OFFSET = 0;
+
+const getComments = ({
+  slug,
+  limit = LIMIT,
+  offset = OFFSET,
+}: {
+  slug: string;
+  limit: number;
+  offset: number;
+}): Promise<CommentDetailsResponse> => {
+  return api.get(`/articles/${slug}/comments`, {
+    params: {
+      limit,
+      offset,
+    },
+  });
+};
+
+const getInfiniteCommentsQueryOptions = (slug: string) => {
+  return infiniteQueryOptions({
+    queryKey: ['comments', slug],
+    queryFn: ({ pageParam = 1 }) => {
+      const limit = LIMIT;
+      const offset = (pageParam - 1) * limit;
+      return getComments({ slug, limit, offset });
+    },
+    getNextPageParam: (lastPage) => {
+      const limit = lastPage.limit;
+      const offset = lastPage.offset;
+      const total = lastPage.total;
+      if (offset + limit === total) return undefined;
+      const nextPage = offset / limit + 2;
+      return nextPage;
+    },
+    initialPageParam: 1,
+  });
+};
+
+type UseCommentsOptions = {
+  slug: string;
+  limit?: number;
+  offset?: number;
+  queryConfig?: QueryConfig<typeof getComments>;
+};
+
+export const useInfiniteComments = ({ slug }: UseCommentsOptions) => {
+  return useInfiniteQuery({
+    ...getInfiniteCommentsQueryOptions(slug),
+  });
+};
