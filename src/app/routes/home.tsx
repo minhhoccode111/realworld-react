95efb48d5c6ae1c4bab325f36f80934fbc067b1e
minhhoccode111 <minhhoccode111@gmail.com>
2025-11-23 14:03:03 +0700

    add: clientLoader for tags in home page

--- src/app/routes/home.tsx
+++ src/app/routes/home.tsx
@@ -1,7 +1,18 @@
 import { AppLayout } from '@/components/layouts/app-layout';
 import { ArticlesLayout } from '@/components/layouts/articles-layout';
 import { HomeLayout } from '@/components/layouts/home-layout';
+import { getInfiniteTagsQueryOptions } from '@/features/tags/api/get-tags';
 import { TagsList } from '@/features/tags/components/tags-list';
+import { QueryClient } from '@tanstack/react-query';
+
+export const clientLoader = (queryClient: QueryClient) => async () => {
+  const tagsQuery = getInfiniteTagsQueryOptions({});
+
+  return (
+    queryClient.getQueryData(tagsQuery.queryKey) ??
+    (await queryClient.fetchInfiniteQuery(tagsQuery))
+  );
+};
 
 const HomeRoute = () => {
   return (
