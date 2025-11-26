58dd24f4d95b1222ce3e39e187231a3beedc85ee
minhhoccode111 <minhhoccode111@gmail.com>
2025-11-26 20:44:28 +0700

    add: clientLoader for home page get-articles global

--- src/app/routes/home.tsx
+++ src/app/routes/home.tsx
@@ -1,18 +1,36 @@
 import { AppLayout } from '@/components/layouts/app-layout';
 import { ArticlesLayout } from '@/components/layouts/articles-layout';
 import { HomeLayout } from '@/components/layouts/home-layout';
+import { LIMIT_DEFAULT } from '@/config/constants';
+import { getArticlesQueryOptions } from '@/features/articles/api/get-articles';
 import { getInfiniteTagsQueryOptions } from '@/features/tags/api/get-tags';
 import { TagsList } from '@/features/tags/components/tags-list';
 import { QueryClient } from '@tanstack/react-query';
+import { LoaderFunctionArgs } from 'react-router';
 
-export const clientLoader = (queryClient: QueryClient) => async () => {
-  const tagsQuery = getInfiniteTagsQueryOptions({});
+export const clientLoader =
+  (queryClient: QueryClient) =>
+  async ({ request }: LoaderFunctionArgs) => {
+    const url = new URL(request.url);
+    const page = Number(url.searchParams.get('page') || 1);
 
-  return (
-    queryClient.getQueryData(tagsQuery.queryKey) ??
-    (await queryClient.fetchInfiniteQuery(tagsQuery))
-  );
-};
+    const tagsQuery = getInfiniteTagsQueryOptions({});
+    const articlesQuery = getArticlesQueryOptions({
+      isFeed: false,
+      author: '',
+      favorited: '',
+      tag: '',
+      limit: LIMIT_DEFAULT,
+      offset: (page - 1) * LIMIT_DEFAULT,
+    });
+
+    return [
+      queryClient.getQueryData(tagsQuery.queryKey) ??
+        (await queryClient.fetchInfiniteQuery(tagsQuery)),
+      queryClient.getQueryData(articlesQuery.queryKey) ??
+        (await queryClient.fetchQuery(articlesQuery)),
+    ];
+  };
 
 const HomeRoute = () => {
   return (
