d7bb52ac662467f36c4a2c2cb46d79f264243e96
minhhoccode111 <minhhoccode111@gmail.com>
2025-12-01 21:46:03 +0700

    add: clientLoader for editor-update

--- src/app/routes/editor/update.tsx
+++ src/app/routes/editor/update.tsx
@@ -1,6 +1,21 @@
-import { useParams } from 'react-router';
+import { QueryClient } from '@tanstack/react-query';
+import { LoaderFunctionArgs, useParams } from 'react-router';
 
+import { getArticleQueryOptions } from '@/features/articles/api/get-article';
 import { UpdateArticleForm } from '@/features/articles/components/update-article-form';
+
+export const clientLoader =
+  (queryClient: QueryClient) =>
+  async ({ params }: LoaderFunctionArgs) => {
+    const slug = params.slug as string;
+    const articleQuery = getArticleQueryOptions({ slug });
+
+    const article =
+      queryClient.getQueryData(articleQuery.queryKey) ??
+      (await queryClient.fetchQuery(articleQuery));
+
+    return { article };
+  };
 
 const EditorUpdateRoute = () => {
   const params = useParams();
